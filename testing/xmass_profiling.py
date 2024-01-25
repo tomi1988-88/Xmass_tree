b56c8dbd02a00ada0f921c7f70de03b744aa17fe
tomi1988-88 <62662726+tomi1988-88@users.noreply.github.com>
2024-01-25 15:33:04 +0100

    Update xmass_profiling.py
    profiling

--- testing/xmass_profiling.py
+++ testing/xmass_profiling.py
@@ -4,6 +4,7 @@ from Xmass_tree import tree_1
 from Xmass_tree import tree_2
 from Xmass_tree import tree_3
 from Xmass_tree import TreeObj
+from Xmass_tree import replace_with_interval
 
 
 def tree_1_lst(lines: int) -> str:
@@ -21,27 +22,69 @@ def tree_1_lst(lines: int) -> str:
 
     return "".join(tree_lst[:-1])
 
-# print(tree_1(5))
-# print(tree_1_lst(5))
+
+def replace_with_interval_str_cut(tree_str: str, interval: int) -> str:
+    if interval == 1:
+        return tree_str  # changed tree_str.replace("$", "O")
+
+    num_of_dec = tree_str.count("O")  # changed "$"
+
+    # Altered part to the end:
+    result = ""
+    temp = tree_str
+
+    for i in range(1, num_of_dec + 1):
+        ind_O = temp.find("O") + 1
+        if i % interval == 1:
+            result += temp[:ind_O]
+            temp = temp[ind_O:]
+        else:
+            result += temp[:ind_O - 1] + "*"
+            temp = temp[ind_O:]
+
+    result += temp
+
+    return result
 
 
-# cProfile.run("tree_1(5)")
-# cProfile.run("tree_1_lst(5)")
-# cProfile.run("tree_2(5)")
-# cProfile.run("tree_3(5, 1)")
-# cProfile.run("tree_3(5, 3)")
+def tree_3_str_cut(lines: int, interval: int) -> str:
+    stars = 1
+    spaces = lines
+
+    tree_str = f"{' ' * (lines - 1)}X\n" + f"{' ' * (lines - 1)}^\n" + f"{' ' * (spaces - 2)}/*\\\n"
+
+    while spaces > 2:
+        tree_str += f"{' ' * (spaces - 3)}/{'*O' * stars}*\\\n"  # changed {'*$' * stars} to {'*O' * stars}
+        stars += 1
+        spaces -= 1
+
+    tree_str += f"{' ' * (lines - 2)}| |"
+
+    tree_str = replace_with_interval_str_cut(tree_str, interval)  # changed replace_with_interval to replace_with_interval_amend
+
+    return tree_str
+
+
+cProfile.run("tree_1(5)")
+print(timeit.repeat("tree_1(5)", repeat=3, globals=globals()))
+
+cProfile.run("tree_1_lst(5)")
+print(timeit.repeat("tree_1_lst(5)", repeat=3, globals=globals()))
+
+cProfile.run("tree_2(5)")
+print(timeit.repeat("tree_2(5)", repeat=3, globals=globals()))
+
+cProfile.run("tree_3(5, 1)")
+print(timeit.repeat("tree_3(5, 1)", repeat=3, globals=globals()))
+
+cProfile.run("tree_3(5, 3)")
+print(timeit.repeat("tree_3(5, 3)", repeat=3, globals=globals()))
+
+cProfile.run("tree_3_amend(5, 3)")
+print(timeit.repeat("tree_3_amend(5, 3)", repeat=3, globals=globals()))
+
 cProfile.run("TreeObj(1, 5, 1, 1, 1).tree_raw")
+print(timeit.repeat("TreeObj(1, 5, 1, 1, 1).tree_raw", repeat=3, globals=globals()))
+
 cProfile.run("TreeObj(1, 5, 3, 1, 1).tree_raw")
-
-
-# print(globals())
-# print(locals())
-# print(timeit.repeat("tree_1(5)", repeat=3, globals=globals()))
-# print(timeit.repeat("tree_1_lst(5)", repeat=3, globals=globals()))
-
-# print(timeit.repeat("tree_2(5)", repeat=3, globals=globals()))
-# print(timeit.repeat("tree_3(5, 1)", repeat=3, globals=globals()))
-# print(timeit.repeat("tree_3(5, 3)", repeat=3, globals=globals()))
-# print(timeit.repeat("TreeObj(1, 5, 1, 1, 1).tree_raw", repeat=3, globals=globals()))
 print(timeit.repeat("TreeObj(1, 5, 3, 1, 1).tree_raw", repeat=3, globals=globals()))
-
